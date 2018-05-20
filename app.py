f7f9625c96e0db46a4dbb778433a77af916e3377
Moritz Haller <haller.moritz@gmail.com>
2018-05-20 14:16:24 +0100

    add homepage and integrate typeform

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from datetime import datetime
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "hello world"
+	title = 'Privacypilot'
+	return render_template('home.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
